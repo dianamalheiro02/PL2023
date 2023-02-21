9e705ca125001ee98f26eddcd3f34f937c2ec564
dianamalheiro02 <73439554+dianamalheiro02@users.noreply.github.com>
2023-02-21 12:26:47 +0000

    Update main.py

--- TPC2/main.py
+++ TPC2/main.py
@@ -1,1 +1,26 @@
 #Resposta a TPC2
+string = input("Somador ON/OFF:\n")
+
+string = string.rstrip().split(' ')
+
+soma=0
+on=0
+
+for element in string:
+    if element == "ON" or element == "on":
+        on = 1
+
+    if element == "OFF" or element == "off":
+        on = 0
+        soma = 0
+
+    if element.isnumeric() == True:
+        if on == 1:
+            soma = soma + int(element)
+
+    if element == "=":
+        if on == 1:
+            print("Soma: %d" % soma)
+            soma = 0
+        else:
+            print("Sem operações a realizar.")
